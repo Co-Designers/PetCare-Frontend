9ed28f91b8996ac43c73a8c1339287a262baa91c
Ghiou Justinn Mauricio Silva <gjustinnmauricios001@gmail.com>
2026-06-15 16:57:17 -0500

    feat(environments): update api base url in production.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+  platformProviderApiBaseUrl: 'https://petcare-frontend.azurewebsites.net/api/v1',
   platformProviderSignInEndpointPath: '/authentication/sign-in',
   platformProviderSignUpEndpointPath: '/authentication/sign-up',
 };
